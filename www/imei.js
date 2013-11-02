7adba9c389a742a181dc9dc167201065c5d3327b
karol2312 <karol2312@wp.pl>
2013-11-02 21:18:12 +0100

    ad

--- www/imei.js
+++ www/imei.js
@@ -1,14 +1,19 @@
 var IMEIplugin = { 
-    getIMEI: function (success, fail, resultType) {
-    
-    var platform = device.platform;
+     
+}
+
+IMEIplugin.prototype.getIMEI = function(name, successCallback, failureCallback) { 
+          var platform = device.platform;
     	if(platform == 'Android'){
-    	 	return cordova.exec( success, fail, 
-                         	  "com.GSMstock.IMEI", 
+    	 	return Phonegap.exec( success, fail, 
+                         	  "com.GSMstock.IMEIplugin", 
                          	  "", [resultType]); 
          }  else if(platform == 'BlackBerry'){
              	return blackberry.identity.IMEI;
     	}
-    }
-     
-};
+}; 
+
+
+PhoneGap.addConstructor(function() { 
+  PhoneGap.addPlugin('com.GSMstock.IMEIplugin', new IMEIplugin()); 
+}); 
