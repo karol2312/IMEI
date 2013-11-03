f582ab3fba9db17e17dd0e34de8410a561eed2be
karol2312 <karol2312@wp.pl>
2013-11-03 01:40:25 +0100

    c

--- www/imei.js
+++ www/imei.js
@@ -1,13 +1,13 @@
 var IMEIplugin = { 
      
-}
+};
 
 IMEIplugin.prototype.getIMEI = function(success, fail) { 
           var platform = device.platform;
     	if(platform == 'Android'){
     	 	return Phonegap.exec( success, fail, 
-                         	  "com.GSMstock.IMEIplugin", 
-                         	  "", [resultType]); 
+                         	  'com.GSMstock.IMEIplugin', 
+                         	  '', [resultType]); 
          }  else if(platform == 'BlackBerry'){
              	return blackberry.identity.IMEI;
     	}
